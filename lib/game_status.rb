8019f36f99b1260508930a7e5c8d33dd8cc018a9
Cihan Akman <cihanozanakman@gmail.com>
2018-11-02 20:42:15 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,30 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won(board)
+  if  board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    return false
+  else
+    types = ["X", "O"]
+    types.each do |type|
+      WIN_COMBINATIONS.each do |win_comb|
+        if board[win_comb[0]] == type && board[win_comb[1]] == type && board[win_comb[2]] == type
+          return true
+        else
+          return false
+        end
+      end
+    end
+  end
+end
